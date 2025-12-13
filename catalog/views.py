7384f6ba388f740b5856dc1b11bfab576dc8312f
Алексей <denaz2020@mail.ru>
2025-12-14 01:18:07 +0800

    созданы новые контролер и шаблон

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from catalog.models import Product
+
+
+def index(request):
+    product_list = Product.objects.all()
+    # Pagination with 3 products per page
+    paginator = Paginator(product_list, 3)
+    page_number = request.GET.get("page", 1)
+    products = paginator.page(page_number)
+
+    context = {"products": products}
+    return render(request, "index.html", context=context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context=context)
 
 
 def home(request):
