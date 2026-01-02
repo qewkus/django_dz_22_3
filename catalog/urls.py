2e1b33c1a692eff8bfd1045e82b1fa05c068cc40
Алексей <denaz2020@mail.ru>
2026-01-02 21:20:20 +0800

    add forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import AppNameConfig
-from . import views
+from catalog.views import ProductListView, ProductCreateView, ProductUpdateView, VersionListView, \
+    ProductDelete, ProductDetailView
 
 app_name = AppNameConfig.name
 
 urlpatterns = [
-    path("", views.ProductListView.as_view(), name="index"),
-    path("contacts/", views.ContactsView.as_view(), name="contacts"),
-    path("products/<int:pk>/", views.ProductDetailView.as_view(), name="product_details"),
+    path('', cache_page(60)(home), name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/', ProductListView.as_view(), name='product'),
+    path('create/', ProductCreateView.as_view(), name='create_product'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('', VersionListView.as_view(), name='list'),
+    path('delete/<int:pk>/', ProductDelete.as_view(), name='delete_product'),
+    path('view/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
 ]
