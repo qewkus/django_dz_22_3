1f7d6acb3bfe522d9c65101c1023e96a41ca15f1
Алексей <denaz2020@mail.ru>
2026-01-17 18:37:52 +0800

    add code

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,14 +8,33 @@ from catalog.views import (
     ProductUpdateView,
 )
 from django.urls import path
+from django.views.decorators.cache import cache_page
+from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", CatalogHomeView.as_view(), name="home"),
-    path("contacts/", CatalogContactsView.as_view(), name="contacts"),
-    path("product_detail/<int:pk>/", CatalogDetailView.as_view(), name="product_detail"),
-    path("product_create/", ProductCreateView.as_view(), name="product_create"),
-    path("product_update/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
-    path("product_delete/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
+    path("", views.ProductListView.as_view(), name="index"),
+    path("contacts/", views.ContactsView.as_view(), name="contacts"),
+    path(
+        "products/<int:pk>/",
+        cache_page(60)(views.ProductDetailView.as_view()),
+        name="product_details",
+    ),
+    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path(
+        "products/update/<int:pk>/",
+        views.ProductUpdateView.as_view(),
+        name="product_update",
+    ),
+    path(
+        "products/delete/<int:pk>/",
+        views.ProductDeleteView.as_view(),
+        name="product_delete",
+    ),
+    path(
+        "category/<int:pk>/",
+        views.CategoryProductListView.as_view(),
+        name="category_products",
+    ),
 ]
